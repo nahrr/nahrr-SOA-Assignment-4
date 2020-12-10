6914a380e2cdfddae41b866bb1a62f3b9d207f5b
Johan <Johan@DESKTOP-G5FFIEQ>
2020-12-10 15:29:28 +0100

    JS function added

--- Assignment4/wwwroot/js/site.js
+++ Assignment4/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let courses = [];
+const searchBar = document.getElementById('searchBar');
+
+searchBar.addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+});
