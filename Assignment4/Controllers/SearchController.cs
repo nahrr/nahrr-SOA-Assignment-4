a379ef84f2bd91fab80d01914ca1f63058f336d8
Johan <Johan@DESKTOP-G5FFIEQ>
2020-12-10 22:41:58 +0100

    JS

--- Assignment4/Controllers/SearchController.cs
+++ Assignment4/Controllers/SearchController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace Assignment4.Controllers
 {
-    public class SearchController
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+
+    public class SearchController : ControllerBase
     {
         private string search;
+
+        
         public SearchController(string search)
         {
-            this.search = search;
+           this.search = search;
+        }
+
+
+        [Route("{courseCode}")]
+        [HttpGet]
+        public string GetCourseCode(string courseCode)
+        {
+            if (string.IsNullOrEmpty(courseCode))
+            {
+                throw new ArgumentException($"'{nameof(courseCode)}' cannot be null or empty", nameof(courseCode));
+            }
+
+            return courseCode;
         }
 
     }
--- Assignment4/wwwroot/js/site.js
+++ Assignment4/wwwroot/js/site.js
@@ -2,10 +2,67 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//document.getElementById('searchBar').onkeypress = function (e) {
+//    if (e.keyCode == 13) {
+//        document.getElementById('searchBar').click();
+//    }
+//}
 
-let courses = [];
-const searchBar = document.getElementById('searchBar');
 
-searchBar.addEventListener('keyup', (e) => {
-    console.log(e.target.value);
-});
+function searchValue() {
+
+        var searchWord = document.getElementById('searchBar').value;
+       
+
+      //  var jsonSearch = JSON.stringify(search);
+      //  var url = "/controller/searchcontroller"
+        sendValue(searchWord);
+}   
+
+function sendValue(searchObj) {
+
+  
+    var search = searchObj;
+    //alert(search)
+    //$.ajax({
+    //    traditional: true,
+    //    type: "POST",
+    //    url: "http://localhost:44387/api/Search/" + search,
+    //    data: {searchObj: search},
+    // //   contentType: "application/json; charset=utf-8",
+    //    dataType: "json",
+    //    success: function (response) {
+    //        if (response != null) {
+    //            alert("GG EZ");
+    //        } else {
+    //            alert("Something went wrong");
+    //        }
+    //    },
+    //    failure: function (response) {
+    //        alert("failure " + search);
+    //    },
+    //    error: function (response) {
+    //        alert("error " + search);
+    //    }
+    //});  
+
+    $.ajax({
+        url: "http://localhost:44387/api/Search/" + search, // Url of backend (can be python, php, etc..)
+        type: "POST", // data type (can be get, post, put, delete)
+        data: search, // data in json format
+        async: false, // enable or disable async (optional, but suggested as false if you need to populate data afterwards)
+        success: function (response, textStatus, jqXHR) {
+            console.log(response);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+}
+
+
+
+
+
