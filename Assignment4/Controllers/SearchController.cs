da138de193398bac103822bd555828cdb3291221
Anton D <anton.drugge@gmail.com>
2020-12-10 15:19:02 +0100

    la till searchcontroller

---
+++ Assignment4/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment4.Controllers
+{
+    public class SearchController
+    {
+        private string search;
+        public SearchController(string search)
+        {
+            this.search = search;
+        }
+
+    }
+}
