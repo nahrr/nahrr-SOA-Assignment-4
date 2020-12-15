935031dda5bcacbb18f81445b69d03819f4026dd
Anton D <anton.drugge@gmail.com>
2020-12-15 19:48:32 +0100

    Mindre fix

--- Assignment4/Controllers/SearchController.cs
+++ Assignment4/Controllers/SearchController.cs
@@ -18,42 +18,50 @@ namespace Assignment4.Controllers
     {
 
         public string searchUrl = "";
+        public string objectId;
 
         [Route("{courseCode}")]
         [HttpPost]
-        //Tar sökinput från frontend
-        public string GetSearchInput(string courseCode)
-        {
-            if (string.IsNullOrEmpty(courseCode))
-            {
-                throw new ArgumentException($"'{nameof(courseCode)}' cannot be null or empty", nameof(courseCode));
-            }
+        //Tar sökinput från frontend (DENNA KOD GÖR INGENTING NU, FIXA SÅ DEN NÅS FRÅN .JS-FILEN O KÖR FELHANTERING HÄR ELLER KÖR FELHANTERING HELT I .JS-FILEN)
+        //public string GetSearchInput(string courseCode)
+        //{
+        //    if (string.IsNullOrEmpty(courseCode))
+        //    {
+        //        throw new ArgumentException($"'{nameof(courseCode)}' cannot be null or empty", nameof(courseCode));
+        //    }
 
-            return GetSearchCourse(courseCode);
-        }
+        //    return GetSearchCourse(courseCode);
+        //}
 
         //Bygger sök-url efter kurskod
         public string GetSearchCourse(string courseCode)
         {
-            //pepeHacker
             string sourceUrl = "https://cloud.timeedit.net/ltu/web/schedule1/objects.txt?max=15&fr=t&partajax=t&im=f&sid=3&l=sv_SE&search_text=course&types=28";
-
             searchUrl = sourceUrl.Replace("course", courseCode);
-            GetObjectIdByCourseCode(searchUrl);
-            return "";
+
+            return GetObjectIdByCourseCode(searchUrl);
         }
 
         //Tar ut objectid för aktuell kurs från JSON-data (första träffen i records)
         public string GetObjectIdByCourseCode(string searchUrl)
         {
             var jsonData = new WebClient().DownloadString(searchUrl);
-
             var userObj = JObject.Parse(jsonData);
-            string objectId = userObj.SelectToken("records[0].idAndType")
+
+            objectId = userObj.SelectToken("records[0].idAndType")
                 .ToString();
 
-            return objectId;
+            return GetScheduleByObjectId(objectId);
         }
+
+        //Hämtar schema via objekt-id
+        public string GetScheduleByObjectId(string objectId)
+        {
+            var url = "https://cloud.timeedit.net/ltu/web/schedule1/ri.html?h=t&sid=3&p=20200901.x,20300117.x&objects=insertObj&ox=0&types=0&fe=0";
+            var correctUrl = url.Replace("insertObj", objectId);
+            return correctUrl;
+        }
+
     }
 }
 
