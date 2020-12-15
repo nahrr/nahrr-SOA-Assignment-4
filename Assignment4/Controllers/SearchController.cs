ca1e9cfb2c4a396ae7136a6c694602c2b011da88
Johan <Johan@DESKTOP-G5FFIEQ>
2020-12-15 15:36:19 +0100

    url builder added

--- Assignment4/Controllers/SearchController.cs
+++ Assignment4/Controllers/SearchController.cs
@@ -17,15 +17,33 @@ namespace Assignment4.Controllers
        
         [Route("{courseCode}")]
         [HttpPost]
-        public string GetCourseCode(string courseCode)
+        //Tar sökinput från frontend
+        public string GetSearchInput(string courseCode)
         {
             if (string.IsNullOrEmpty(courseCode))
             {
                 throw new ArgumentException($"'{nameof(courseCode)}' cannot be null or empty", nameof(courseCode));
             }
 
-            return $"Det fungerar" + courseCode;
+            return GetSearchCourse(courseCode);
+        }
+
+        //Bygger sök-url efter kurskod
+        public string GetSearchCourse(string courseCode)
+        {
+            //pepeHacker
+            string sourceUrl = "https://cloud.timeedit.net/ltu/web/schedule1/objects.txt?max=15&fr=t&partajax=t&im=f&sid=3&l=sv_SE&search_text=course&types=28";
+
+            var searchUrl = sourceUrl.Replace("course", courseCode);
+
+            return "";
+        }
+
+        public string GetObjectIdByCourseCode(string courseCode)
+        {
+            return "";
         }
 
     }
 }
+
