f64b2db0a703e6678b12287e07968c86b47c1bc1
Johan <Johan@DESKTOP-G5FFIEQ>
2021-01-08 23:04:48 +0100

    Time zone utc+1 added

--- Assignment4/Controllers/PostToCanvasController.cs
+++ Assignment4/Controllers/PostToCanvasController.cs
@@ -12,31 +12,34 @@ namespace Assignment4.Controllers
     [ApiController]
     public class PostToCanvasController : ControllerBase
     {
-        private const string Z = "Z";
+        private const string Z = "+01:00";//TimeZone utc+1
         private const string T = "T:";
+        private const string token = "Bearer 3755~YPKPwpVYGZxHgMBOur8V4akVQ5vCsOWBCThnTtiazZEGsBwUN8dQ7yW9j1ycwlzR";//Johans Token
 
         [Route("{title}/{date}/{startTime}/{endTime}/{comment}")]
         [HttpGet]
-        //Lite kladd, men det fungerar att skapa ett pass i Canvas, title är dynamisk. 
-        public string  GetCanvasData(string title, string date, string startTime, string endTime, string comment)
+        //Ändra return?
+        public string GetCanvasData(string title, string date, string startTime, string endTime, string comment)
         {
+      
             _ = PostCanvasAsync(title, date, startTime, endTime, comment);
             return "";
         }
+        //Postar till Canvas
         private async Task PostCanvasAsync(string title, string date, string startTime, string endTime, string comment)
         {
             using (var httpClient = new HttpClient())
             {
                 using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://ltu.instructure.com/api/v1/calendar_events.json"))
                 {
-                    request.Headers.TryAddWithoutValidation("Authorization", "Bearer 3755~YPKPwpVYGZxHgMBOur8V4akVQ5vCsOWBCThnTtiazZEGsBwUN8dQ7yW9j1ycwlzR");
+                    request.Headers.TryAddWithoutValidation("Authorization", token);
 
                     var multipartContent = new MultipartFormDataContent();
                     multipartContent.Add(new StringContent("user_66806"), "calendar_event[context_code]");
                     multipartContent.Add(new StringContent(title), "calendar_event[title]");
                     multipartContent.Add(new StringContent(date + T + startTime + Z), "calendar_event[start_at]");
                     multipartContent.Add(new StringContent(date + T + endTime + Z), "calendar_event[end_at]");
-                    multipartContent.Add(new StringContent(comment), "calendar_event[description]"); 
+                    multipartContent.Add(new StringContent(comment), "calendar_event[description]");
                     request.Content = multipartContent;
 
                     var response = await httpClient.SendAsync(request);
